f11656e7e8efc4725577aaa10dd718c73e619b0b
Mujtaba Abid <mujtabaabid06@gmail.com>
2026-03-25 13:15:30 +0500

    make api/index for vercel

---
+++ server/api/index.js
@@ -0,0 +1,27 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './app.js';
+import connectDB from './config/db.js';
+import configurePassport from './config/passport.js';
+import initCronJobs from './jobs/resetLimits.js';
+
+let isInitialized = false;
+
+export default async function handler(req, res) {
+    try {
+        await connectDB(); // 🔥 har request pe ensure connection
+
+        if (!isInitialized) {
+            configurePassport(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET);
+            initCronJobs();
+            isInitialized = true;
+        }
+
+        return app(req, res); // express app handle karega
+    } catch (error) {
+        console.error("❌ SERVER ERROR:", error);
+        res.status(500).json({ message: error.message });
+    }
+}
