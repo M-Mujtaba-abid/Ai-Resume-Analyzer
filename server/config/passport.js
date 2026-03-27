2427a6117ab7d6642096c514139f10993ec73744
Mujtaba Abid <mujtabaabid06@gmail.com>
2026-03-27 11:54:59 +0500

    add proxy true for vercel

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,39 +4,43 @@ import passport from "passport";
 
 // Ek function banayein jo app.js se call hoga
 const configurePassport = () => {
-    passport.use(new GoogleStrategy({
+  passport.use(
+    new GoogleStrategy(
+      {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/user/google/back" // Route check karlein (app.js mein /user prefix hai)
+        callbackURL: "/user/google/back", // Route check karlein (app.js mein /user prefix hai)
+        proxy: true,
       },
-      async ( accessToken, refreshToken,profile, done) => {
+      async (accessToken, refreshToken, profile, done) => {
         try {
-            let user = await User.findOne({ email: profile.emails[0].value });
-    
-            if (user) {
-                if (!user.googleId) {
-                    user.googleId = profile.id;
-                    user.avatar = profile.photos[0].value;
-                    user.isEmailVerified = true;
-                    await user.save({ validateBeforeSave: false });
-                }
-                return done(null, user);
+          let user = await User.findOne({ email: profile.emails[0].value });
+
+          if (user) {
+            if (!user.googleId) {
+              user.googleId = profile.id;
+              user.avatar = profile.photos[0].value;
+              user.isEmailVerified = true;
+              await user.save({ validateBeforeSave: false });
             }
-    
-            user = await User.create({
-                name: profile.displayName,
-                email: profile.emails[0].value,
-                googleId: profile.id,
-                avatar: profile.photos[0].value,
-                isEmailVerified: true,
-            });
-    
             return done(null, user);
+          }
+
+          user = await User.create({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id,
+            avatar: profile.photos[0].value,
+            isEmailVerified: true,
+          });
+
+          return done(null, user);
         } catch (error) {
-            return done(error, null);
+          return done(error, null);
         }
-      }
-    ));
-}
+      },
+    ),
+  );
+};
 
 export default configurePassport;
