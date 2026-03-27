6c809e43c43259568894f4b3b4b11e5268260ffb
Mujtaba Abid <mujtabaabid06@gmail.com>
2026-03-27 11:25:34 +0500

    add these two line  prompt: select_account consent, access_type: offline

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,26 +1,48 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken, forgotPassword, resetPassword, verifyEmail, changeCurrentPassword, googleAuthCallback, getCurrentUser } from "../controllers/user.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  forgotPassword,
+  resetPassword,
+  verifyEmail,
+  changeCurrentPassword,
+  googleAuthCallback,
+  getCurrentUser,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import passport from "passport";
 
 const router = Router();
 
-router.post("/register",registerUser);
-router.post("/login",loginUser);
-router.post("/refreshAccessToken",refreshAccessToken);
-router.post("/forgotPassword",forgotPassword);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/refreshAccessToken", refreshAccessToken);
+router.post("/forgotPassword", forgotPassword);
 router.post("/resetPassword/:token", resetPassword);
 router.post("/verifyEmail", verifyEmail);
 router.post("/changeCurrentPassword", verifyJWT, changeCurrentPassword);
 router.post("/logout", verifyJWT, logoutUser);
 
-router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    prompt: "select_account consent",
+    access_type: "offline",
+  }),
+);
 
 // 2. Google Callback (Jahan Google wapas bhejega)
-router.get("/google/back",
-    passport.authenticate("google", { session: false, failureRedirect: "/login" }),
-    googleAuthCallback // Clean controller call
-);;
+router.get(
+  "/google/back",
+  passport.authenticate("google", {
+    session: false,
+    failureRedirect: "/login",
+  }),
+  googleAuthCallback, // Clean controller call
+);
 
 router.get("/getCurrentUser", verifyJWT, getCurrentUser);
 
